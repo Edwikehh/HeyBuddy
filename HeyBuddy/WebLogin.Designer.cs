a08083e74bb4b7ba55cd7c09aa446455b93a37d9
Edwikehh <edwikeh@gmail.com>
2020-02-13 20:49:54 +0100

    timer

--- HeyBuddy/WebLogin.Designer.cs
+++ HeyBuddy/WebLogin.Designer.cs
@@ -33,6 +33,7 @@
             this.btnWebLogin = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.popUp = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnWebConfirm = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // geckoWebBrowser1
@@ -49,13 +50,11 @@
             // 
             // btnWebLogin
             // 
-            this.btnWebLogin.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.btnWebLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnWebLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(47)))), ((int)(((byte)(61)))));
-            this.btnWebLogin.Location = new System.Drawing.Point(339, 5);
+            this.btnWebLogin.Location = new System.Drawing.Point(12, 6);
             this.btnWebLogin.Name = "btnWebLogin";
-            this.btnWebLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnWebLogin.Size = new System.Drawing.Size(354, 23);
             this.btnWebLogin.TabIndex = 1;
             this.btnWebLogin.Text = "button1";
             this.btnWebLogin.UseVisualStyleBackColor = true;
@@ -73,10 +72,25 @@
             this.popUp.Text = "notifyIcon1";
             this.popUp.Visible = true;
             // 
+            // btnWebConfirm
+            // 
+            this.btnWebConfirm.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnWebConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWebConfirm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(47)))), ((int)(((byte)(61)))));
+            this.btnWebConfirm.Location = new System.Drawing.Point(384, 5);
+            this.btnWebConfirm.Name = "btnWebConfirm";
+            this.btnWebConfirm.Size = new System.Drawing.Size(354, 23);
+            this.btnWebConfirm.TabIndex = 3;
+            this.btnWebConfirm.Text = "button1";
+            this.btnWebConfirm.UseVisualStyleBackColor = true;
+            this.btnWebConfirm.Click += new System.EventHandler(this.btnWebConfirm_Click);
+            // 
             // WebLogin
             // 
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(216)))), ((int)(((byte)(220)))));
             this.ClientSize = new System.Drawing.Size(750, 454);
+            this.Controls.Add(this.btnWebConfirm);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.geckoWebBrowser1);
             this.Controls.Add(this.btnWebLogin);
@@ -93,5 +107,6 @@
         private System.Windows.Forms.Button btnWebLogin;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.NotifyIcon popUp;
+        private System.Windows.Forms.Button btnWebConfirm;
     }
 }
--- HeyBuddy/WebLogin.cs
+++ HeyBuddy/WebLogin.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 using Gecko;
+using Timer = System.Timers.Timer;
+
+
+
 
 namespace HeyBuddy
 {
     public partial class WebLogin : Form
     {
-        string appLoc = ".HeyBuddy/log";
+        string appLoc = ".HeyBuddy/log/";
         string appdataLoc;
 
         string folder
@@ -26,6 +33,9 @@ namespace HeyBuddy
             }
         }
 
+        bool buddyactive = false;
+        int filenum = 2;
+
         public WebLogin()
         {
             appdataLoc = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
@@ -34,6 +44,12 @@ namespace HeyBuddy
             textBox1.Text = "https://account.eslgaming.com/login";
             Xpcom.Initialize("Firefox");
             geckoWebBrowser1.Navigate(textBox1.Text);
+
+
+            btnWebLogin.Text = "Redirect to ESL Admin tickets page";
+            btnWebConfirm.Text = "Active HeyBuddy! system [Please log in first]";
+            btnWebConfirm.Enabled = false;
+
 
         }
 
@@ -47,28 +63,151 @@ namespace HeyBuddy
             popUp.ShowBalloonTip(1000);
         }
 
+
+
         private void WebLogin_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void btnWebLogin_Click(object sender, EventArgs e)
         {
-            WebRequest req = HttpWebRequest.Create("http://play.eslgaming.com/admin_tickets");
-            req.Method = "GET";
+            btnWebConfirm.Enabled = true;
+            textBox1.Text = "http://play.eslgaming.com/admin_tickets";
+            geckoWebBrowser1.Navigate(textBox1.Text);
+            btnWebLogin.Text = "Redirecting...";
+            new System.Threading.Timer((Object stateInfo) => { Console.WriteLine("7 sec"); }, new AutoResetEvent(false), 0, 7000);
+            btnWebLogin.Text = "Done! You can active it!";
 
-            string source;
-            using (StreamReader reader = new StreamReader(req.GetResponse().GetResponseStream()))
+        }
+
+
+        private void GetHtml()
+        {
+            GeckoHtmlElement element = null;
+            var geckoDomElement = geckoWebBrowser1.Document.DocumentElement;
+            if (geckoDomElement is GeckoHtmlElement)
             {
-                source = reader.ReadToEnd();
+                element = (GeckoHtmlElement)geckoDomElement;
+                var innerHtml = element.InnerHtml;
             }
 
-            int first;
-            int second;
-
-
-
-            System.IO.File.WriteAllText(folder + "/test.txt", source);
+            System.IO.File.WriteAllText(folder + "/esl_log" + filenum + ".txt", element.InnerHtml);
+            filenum += 1;
+            
         }
+
+        public void Wait(int ms)
+        {
+            DateTime start = DateTime.Now;
+            while ((DateTime.Now - start).TotalMilliseconds < ms)
+                Application.DoEvents();
+
+        }
+
+        private bool FileCompare(string file1, string file2)
+        {
+            int file1byte;
+            int file2byte;
+            FileStream fs1;
+            FileStream fs2;
+
+            // Determine if the same file was referenced two times.
+            if (file1 == file2)
+            {
+                // Return true to indicate that the files are the same.
+                return true;
+            }
+
+            // Open the two files.
+            fs1 = new FileStream(file1, FileMode.Open, FileAccess.Read);
+            fs2 = new FileStream(file2, FileMode.Open, FileAccess.Read);
+
+            // Check the file sizes. If they are not the same, the files 
+            // are not the same.
+            if (fs1.Length != fs2.Length)
+            {
+                // Close the file
+                fs1.Close();
+                fs2.Close();
+
+                // Return false to indicate files are different
+                return false;
+            }
+
+            // Read and compare a byte from each file until either a
+            // non-matching set of bytes is found or until the end of
+            // file1 is reached.
+            do
+            {
+                // Read one byte from each file.
+                file1byte = fs1.ReadByte();
+                file2byte = fs2.ReadByte();
+            }
+            while ((file1byte == file2byte) && (file1byte != -1));
+
+            // Close the files.
+            fs1.Close();
+            fs2.Close();
+
+            return ((file1byte - file2byte) == 0);
+        }
+
+        private void btnWebConfirm_Click(object sender, EventArgs e)
+        {
+            buddyactive = true;
+
+
+            while(buddyactive)
+            {
+                Wait(5000); // 1 sec = 1000
+                GetHtml();
+
+                string logname = "esl_log";
+                string file0 = folder + logname + filenum + ".txt";
+                string file1 = folder + logname + (filenum - 1)+ ".txt";
+                string file2 = folder + logname + (filenum - 2) + ".txt";
+
+
+
+
+
+
+
+                if (filenum > 2)
+                {
+                    string line = null;
+                    string line_to_delete = "<!--<![endif]";
+
+                    using (StreamReader reader = new StreamReader(file1))
+                    {
+                        using (StreamWriter writer = new StreamWriter(file2))
+                        {
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                if (String.Compare(line, line_to_delete) == 0)
+                                    continue;
+
+                                writer.WriteLine(line);
+                            }
+                        }
+                    }
+
+                    if (FileCompare(file1, file2))
+                    {
+
+                    }
+                    else
+                    {
+                        Notification_Info("New ticket!", "Please check out the new ticket on play.eslgaming.com/admin_tickets");
+                    }
+                }
+
+
+            }
+
+
+        }
+
     }
 }
